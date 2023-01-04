dc8a6567c60e8faa9054d8283405dcb5a03508a6
joneskj55 <joneskj55@gmail.com>
2023-01-04 15:03:40 -0600

    test

--- models/project.js
+++ models/project.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema(
   {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    url: { type: String, required: true },
-    tech: { type: String, required: true },
-    year: {type: String, required: true },
-    deployed: {type: String, required: true },
-    repo: {type: String, required: true },
-    img: {type: String, required: true },
-    private: {type: Boolean, required: false },
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    url: {type: String, required: true},
+    tech: {type: String, required: true},
+    year: {type: String},
+    deployed: {type: String},
+    repo: {type: String},
+    img: {type: String},
+    private: {type: Boolean,},
   },
   {
     collection: "projects",
