eb12fcd45a18ba13b132ced41311491f876833d2
joneskj55 <joneskj55@gmail.com>
2023-01-04 14:59:09 -0600

    update project schema

--- models/project.js
+++ models/project.js
@@ -6,6 +6,12 @@ const ProjectSchema = new Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     url: { type: String, required: true },
+    tech: { type: String, required: true },
+    year: {type: String, required: true },
+    deployed: {type: String, required: true },
+    repo: {type: String, required: true },
+    img: {type: String, required: true },
+    private: {type: Boolean, required: false },
   },
   {
     collection: "projects",
